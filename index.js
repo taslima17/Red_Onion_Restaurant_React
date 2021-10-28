3f150e399bf00aee95a5fed7bad82f81f8804651
Taslima Rumky <taslimarumy17@gmail.com>
2021-10-28 19:29:03 +0600

    second commit

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ async function run() {
 run().catch(console.dir);
 app.get('/', (req, res) => {
     console.log('running get');
-    res.send('helloo there')
+    res.send('hello there')
 });
 app.listen(port, () => {
     console.log('running port', port)
